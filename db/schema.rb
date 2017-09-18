95a90122baa75873b4ab63560e26a85d09823f6d
John Skender <jnskender@gmail.com>
2017-09-18 08:38:13 -0500

    drop table posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915151339) do
+ActiveRecord::Schema.define(version: 20170918133704) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(version: 20170915151339) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "image"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
