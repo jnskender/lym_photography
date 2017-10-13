ce53bae6e6f6dd210f3c49b6487faab03f78d54e
John Skender <jnskender@gmail.com>
2017-10-13 12:40:47 -0500

    Allow thumbnail. Change S3 directory structure to album_title/photo_title

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,16 +1,18 @@
 class ImageUploader < CarrierWave::Uploader::Base
-
   include CarrierWave::MiniMagick
   include Sprockets::Rails::Helper
 
   storage :fog
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.title}"
+    "#{model.album.title}/#{model.title}"
   end
 
   def extension_whitelist
     %w[jpg jpeg gif png]
   end
 
+  version :thumb do
+    process resize_to_fill: [200, 200]
+  end
 end
