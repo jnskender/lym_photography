fa1e2c4092fa5abe20688751dae4aef97a1fc919
John Skender <jnskender@gmail.com>
2017-11-30 09:53:11 -0600

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,15 +74,17 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-ActionMailer::Base.smtp_settings = {
-:user_name => ENV[‘SENDGRID_USERNAME’],
-:password => ENV[‘SENDGRID_PASSWORD’],
-:domain => ‘heroku.com’,
-:address => ‘smtp.sendgrid.net’,
-:port => 587,
-:authentication => :plain,
-:enable_starttls_auto => true
-}
+    config.action_mailer.default_url_options = {host: 'https://lym-photography.herokuapp.com'}
+    config.action_mailer.delivery_method = :smtp
+    ActionMailer::Base.smtp_settings = {
+      :address => 'smtp.sendgrid.net',
+      :port => '587',
+      :authentication => :plain,
+      :user_name => ENV["SENDGRID_USERNAME"],
+      :password => ENV["SENDGRID_PASSWORD"],
+      :domain => 'heroku.com',
+      :enable_starttls_auto => true
+    }
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
