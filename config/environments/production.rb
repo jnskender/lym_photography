a53f55ef40792c5b6bcd4aa04b7cccb3b7e8a3c1
John Skender <jnskender@gmail.com>
2017-11-30 09:44:48 -0600

    remove mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options =  host: ‘https://your app.herokuapp.com’ }
+
   config.action_mailer.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
